6adb7f955c8f66d602e6601ed0a0c505986cedcb
root <601772524@qq.com>
2026-06-01 21:08:44 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -124,8 +124,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.cdlixin.com"];
-pub const RS_PUB_KEY: &str = "oQIW2YPIBhG95+eg8L+Qc79NXbDCRFtrzXILLlFyy8U=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.it520147.top"];
+pub const RS_PUB_KEY: &str = "IIz8cprZQMPFldiDfGj9qmMXJwFFAB74XMxF8nqT0ps=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
