3b00246994a8392dfce2f856337844f028f52d6f
root <601772524@qq.com>
2026-06-02 13:12:39 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -124,7 +124,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.cluster.lixin"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.nb.cn"];
 pub const RS_PUB_KEY: &str = "IIz8cprZQMPFldiDfGj9qmMXJwFFAB74XMxF8nqT0ps=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
